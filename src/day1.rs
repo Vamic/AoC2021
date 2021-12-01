385ae9ea3ad420bd5717b65460a777cfb11c2d0e
Carl König <carl.konig@nordicnest.com>
2021-12-01 10:54:35 +0100

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,29 @@
+use itertools::Itertools;
+
+fn input() -> std::vec::Vec<i32> {
+    let bytes = include_bytes!("./inputs/day1.txt");
+    let str_input = String::from_utf8_lossy(bytes);
+    str_input.lines().map(str::parse).map(Result::unwrap).collect()
+}
+
+pub fn star1() {
+    println!("Day 1 - Star 1");
+    let result = input()
+        .iter()
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count();
+    println!("Result: {}", result);
+}
+
+pub fn star2() {
+    println!("Day 1 - Star 2");
+    let result = input()
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| a < b)
+        .count();
+    println!("Result: {}", result);
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod day1;
+
+fn main() {
+    day1::star1();
+    day1::star2();
+}
