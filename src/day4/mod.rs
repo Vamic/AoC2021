b99074dce1e53a0c53aa13098d3667f89d42ec64
Carl König <carl.konig@nordicnest.com>
2021-12-10 12:11:21 +0100

    Day 4

---
+++ src/day4/mod.rs
@@ -0,0 +1,91 @@
+fn input() -> (Vec<usize>, Vec<Vec<Vec<(usize, bool)>>>) {
+    let bytes = include_bytes!("input.txt");
+    let str_input = String::from_utf8_lossy(bytes);
+    let split = str_input.split_once("\n\n").unwrap();
+    (
+        split
+            .0
+            .split(',')
+            .map(str::parse)
+            .map(Result::unwrap)
+            .collect(),
+        split
+            .1
+            .split("\n\n")
+            .map(|x| {
+                x.lines()
+                    .map(|y| {
+                        y.split_whitespace()
+                            .map(str::parse)
+                            .map(Result::unwrap)
+                            .map(|z| (z, false))
+                            .collect()
+                    })
+                    .collect()
+            })
+            .collect(),
+    )
+}
+
+pub fn star1() {
+    println!("Day 4 - Star 1");
+    let (numbers, mut bingos) = input();
+
+    let mut x_matches = vec![vec![0; bingos[0].len()]; bingos.len()];
+    let mut y_matches = vec![vec![0; bingos[0].len()]; bingos.len()];
+    for num in numbers {
+        for i in 0..bingos.len() {
+            for x in 0..bingos[i].len() {
+                for y in 0..bingos[i][x].len() {
+                    if num == bingos[i][x][y].0 {
+                        bingos[i][x][y].1 = true;
+                        x_matches[i][x] += 1;
+                        y_matches[i][y] += 1;
+                        if y_matches[i][y] == 5 || x_matches[i][x] == 5 {
+                            let unmarked = bingos[i].iter().fold(0, |a, x| {
+                                a + x.iter().filter(|y| !y.1).fold(0, |b, y| b + y.0)
+                            });
+                            println!("Result: {}", num * unmarked);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn star2() {
+    println!("Day 4 - Star 2");
+    let (numbers, mut bingos) = input();
+
+    let mut x_matches = vec![vec![0; bingos[0].len()]; bingos.len()];
+    let mut y_matches = vec![vec![0; bingos[0].len()]; bingos.len()];
+    let mut bingoed_bingos: Vec<usize> = [].to_vec();
+    for num in numbers {
+        for i in 0..bingos.len() {
+            if bingoed_bingos.contains(&i) {
+                continue;
+            }
+            for x in 0..bingos[i].len() {
+                for y in 0..bingos[i][x].len() {
+                    if num == bingos[i][x][y].0 {
+                        bingos[i][x][y].1 = true;
+                        x_matches[i][x] += 1;
+                        y_matches[i][y] += 1;
+                        if y_matches[i][y] == 5 || x_matches[i][x] == 5 {
+                            bingoed_bingos.push(i);
+                            if bingoed_bingos.len() == bingos.len() {
+                                let unmarked = bingos[i].iter().fold(0, |a, x| {
+                                    a + x.iter().filter(|y| !y.1).fold(0, |b, y| b + y.0)
+                                });
+                                println!("Result: {}", num * unmarked);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     day1::star1();
@@ -9,4 +10,6 @@ fn main() {
     day2::star2();
     day3::star1();
     day3::star2();
+    day4::star1();
+    day4::star2();
 }
